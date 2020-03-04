92728db5932ca0b3b5fd9208322c2a4969eb0e79
张雪源 <>
2020-03-04 19:54:27 +0800

    no message

--- urlsafe2/urlsafe.go
+++ urlsafe2/urlsafe.go
@@ -13,13 +13,13 @@ import (
 ** @Last Modified time: 2020-03-04 19:37:54
 ***********************************************/
 
-func base64URLDecode(data string) ([]byte, error) {
+func Base64URLDecode(data string) ([]byte, error) {
 	var missing = (4 - len(data)%4) % 4
 	data += strings.Repeat("=", missing)
 	return base64.URLEncoding.DecodeString(data)
 }
 
-func base64UrlSafeEncode(source []byte) string {
+func Base64UrlSafeEncode(source []byte) string {
 	// Base64 Url Safe is the same as Base64 but does not contain '/' and '+' (replaced by '_' and '-') and trailing '=' are removed.
 	bytearr := base64.StdEncoding.EncodeToString(source)
 	safeurl := strings.Replace(string(bytearr), "/", "_", -1)
